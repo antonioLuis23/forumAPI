1c606cf8e96f9b5b8a3b5de2b6cad24ad1745ea7
Antonio <antonio_luis@outlook.com>
2021-11-22 14:45:42 -0300

    Allowing to register a new topic

--- src/main/java/br/com/alura/forum/modelo/Topico.java
+++ src/main/java/br/com/alura/forum/modelo/Topico.java
@@ -29,13 +29,21 @@ public class Topico {
 	private Curso curso;
 	@OneToMany(mappedBy = "topico")
 	private List<Resposta> respostas = new ArrayList<>();
-	
+
+	public Topico(){}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
+	}
+
+	public Topico(String titulo, String mensagem, Curso curso) {
+		this.titulo = titulo;
+		this.mensagem = mensagem;
+		this.curso = curso;
 	}
 
 	@Override
